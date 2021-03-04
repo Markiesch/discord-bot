6f7624f9329cbc1adc872224ba3f6167c43dfc3b
Markiesch <marksmeel@gmail.com>
2021-03-04 14:09:18 +0100

    reduced cooldown

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
     category: "Fun",
-    cooldown: "5s",
+    cooldown: "3s",
     description: "Test command, to test if the bot is running or not",
     callback: ({ message, client }) => {
         message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
