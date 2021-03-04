e3217376b28384afd619d1e183b2bc6b96cc9bba
Markiesch <marksmeel@gmail.com>
2021-03-04 14:08:01 +0100

    Added Embed command

--- commands/misc/embed.js
+++ commands/misc/embed.js
@@ -1,11 +1,28 @@
 module.exports = {
-    category: "Utility",
+    category: "misc",
     description: "Removes all messages from an Channel",
     guildOnly: true,
+    minArgs: 2,
+    expextedArgs: "<Channel mention> <JSON>",
     requiredPermissions: ["ADMINISTRATOR"],
     callback: ({ message, args }) => {
-        if (!args) {
-            message.channel.send
+        const targetChannel = message.mentions.channels.first() || message.channel;
+
+        if (message.mentions.channels.first()) {
+            // Removes mentioned channel from args array
+            args.shift();
+        }
+
+        try {
+            const json = JSON.parse(args.join(" "));
+            const { text = "" } = json;
+
+            // Sends the embed
+            targetChannel.send(text, {
+                embed: json,
+            });
+        } catch (e) {
+            message.reply("Invalid JSON `" + e.message + "`");
         }
     },
 };
