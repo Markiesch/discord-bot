fbdc31fd8852f857c612fbe54a939b3c2a7c4f27
Markiesch <marksmeel@gmail.com>
2021-03-08 15:28:14 +0100

    New counting feature (experience fase)

---
+++ features/counting.js
@@ -0,0 +1,20 @@
+module.exports = (client) => {
+    client.on("message", (message) => {
+        if (message.author.bot) return;
+        if (message.channel.id != "818487183530393681") return;
+
+        message.channel.messages.fetch({ limit: 2 }).then((res) => {
+            let lm = res.last();
+            // console.log(lm);
+
+            const prevNumber = parseInt(lm.content);
+            const userNumber = parseInt(message.content);
+
+            if (userNumber - prevNumber == 1) {
+                message.react("✅");
+            } else {
+                message.delete();
+            }
+        });
+    });
+};
