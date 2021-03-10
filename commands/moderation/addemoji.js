11d2dc107a974e0aef6a188ae328b686e3cc63e5
Markiesch <marksmeel@gmail.com>
2021-03-10 17:24:55 +0100

    Improved Addemoji command

--- commands/moderation/addemoji.js
+++ commands/moderation/addemoji.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
     category: "Utility",
     description: "Add emojis without opening any menu's",
@@ -6,14 +8,24 @@ module.exports = {
     requiredPermissions: ["MANAGE_EMOJIS"],
     callback: ({ message, args, text }) => {
         const image = message.attachments.first();
-
         if (!image) return message.channel.send("Please attach a image to your message!");
-        if (!text) {
-            message.channel.send("Please provide a name of your new emoji!");
-            return;
+
+        const validFiles = [".png", ".jpeg", ".webp", ".jpg", ".gif"];
+        if (!validFiles.includes(path.parse(image.name).ext.toLocaleLowerCase())) {
+            return message.channel.send("Please upload a `PNG`, `JPEG`, `WEBP`, `JPG` or `GIF` file");
         }
 
-        message.guild.emojis.create(image.url, args[0]).then((emoji) => {
+        let fileName = text || image.name;
+
+        if (!text) {
+            fileName = path.parse(fileName).name;
+        }
+
+        if (fileName == text) {
+            fileName = args[0];
+        }
+
+        message.guild.emojis.create(image.url, fileName).then((emoji) => {
             message.channel.send(`<:${emoji.name}:${emoji.id}> Created new emoji with name **${emoji.name}**!`);
         });
     },
