71a135d0717426c374d374c05795fbff7261d66d
Markiesch <marksmeel@gmail.com>
2021-03-10 16:59:33 +0100

    Added SetIcon command

---
+++ commands/moderation/seticon.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    category: "Utility",
+    description: "Add emojis without opening any menu's",
+    guildOnly: true,
+    requiredPermissions: ["MANAGE_GUILD"],
+    callback: ({ message }) => {
+        const image = message.attachments.first();
+        if (!image) return message.channel.send("Please attach a image to your message!");
+
+        const validFiles = [".png", ".jpeg", ".webp", ".jpg", ".gif"];
+        if (!validFiles.includes(path.parse(image.name).ext.toLocaleLowerCase())) {
+            return message.channel.send("Please upload a `PNG`, `JPEG`, `WEBP`, `JPG` or `GIF` file");
+        }
+
+        message.guild.setIcon(image.url);
+        message.channel.send(new MessageEmbed().setColor("#43b581").setDescription(`<:succes:818800870274891827> Succesfully changed the icon of ${message.guild.name}`));
+    },
+};
