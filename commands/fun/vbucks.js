cb1d3c3c5541baeff42ff182e199b80a80559b66
Markiesch <marksmeel@gmail.com>
2021-03-28 21:46:15 +0200

    Added more responses

--- commands/fun/vbucks.js
+++ commands/fun/vbucks.js
@@ -1,3 +1,5 @@
+const responses = ["virus activated!", "Still trying this?", "https://media.discordapp.net/attachments/751868547491430424/781193784154259466/image0-5.gif"];
+
 module.exports = {
     category: "Fun",
     cooldown: "3s",
@@ -10,7 +12,7 @@ module.exports = {
             const collector = msg.createReactionCollector(filter, { max: 1, time: 15000 });
 
             collector.on("collect", () => {
-                message.channel.send("virus activated!");
+                message.channel.send(responses[Math.floor(Math.random() * responses.length)]);
             });
         });
     },
