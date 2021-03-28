7f842fa784d7292e385cbd7ce8ade1a26cc3f51d
Markiesch <marksmeel@gmail.com>
2021-03-28 17:10:58 +0200

    Added Embeds to Lock command

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -1,19 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     category: "Moderation",
     description: "Makes sure users cant send messages to the mentioned channel",
     expectedArgs: "[channel]",
     guildOnly: true,
     requiredPermissions: ["ADMINISTRATOR"],
-    callback: ({ message, guild }) => {
+    callback: ({ message }) => {
         const targetChannel = message.mentions.channels.first() || message.channel;
+        const failMessage = new MessageEmbed().setColor("#f14948").setDescription(`<:failed:818800981001240617> Failed to lock **<#${targetChannel.id}>**`);
+        const lockedMessage = new MessageEmbed().setColor("#43b581").setDescription(`:lock: **<#${targetChannel.id}>** has been locked`);
 
         // Guild ID is the same as the everyone role ID
         const everyoneID = message.guild.id;
 
-        targetChannel.updateOverwrite(everyoneID, {
-            SEND_MESSAGES: false,
-        });
-
-        targetChannel.send(`**${targetChannel.name}** has been locked :lock:`);
+        try {
+            targetChannel.updateOverwrite(everyoneID, {
+                SEND_MESSAGES: false,
+            });
+            targetChannel.send(lockedMessage);
+        } catch {
+            targetChannel.send(failMessage);
+        }
     },
 };
