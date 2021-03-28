778e938b7c954a377743dfea0e63aa16cd090818
Markiesch <marksmeel@gmail.com>
2021-03-28 21:47:30 +0200

    Added embeds to unlock command

--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     category: "Moderation",
     description: "Makes sure users cant send messages to the mentioned channel",
@@ -6,14 +8,19 @@ module.exports = {
     requiredPermissions: ["ADMINISTRATOR"],
     callback: ({ message }) => {
         const targetChannel = message.mentions.channels.first() || message.channel;
+        const failMessage = new MessageEmbed().setColor("#f14948").setDescription(`<:failed:818800981001240617> Failed to unlock **<#${targetChannel.id}>**`);
+        const lockedMessage = new MessageEmbed().setColor("#43b581").setDescription(`:unlock: **<#${targetChannel.id}>** has been unlocked`);
 
         // Guild ID is the same as the everyone role ID
         const everyoneID = message.guild.id;
 
-        targetChannel.updateOverwrite(everyoneID, {
-            SEND_MESSAGES: true,
-        });
-
-        targetChannel.send(`**${targetChannel.name}** has been unlocked :unlock:`);
+        try {
+            targetChannel.updateOverwrite(everyoneID, {
+                SEND_MESSAGES: true,
+            });
+            targetChannel.send(lockedMessage);
+        } catch {
+            targetChannel.send(failMessage);
+        }
     },
 };
