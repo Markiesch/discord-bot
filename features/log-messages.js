83c5a748b9a88afe4bb3645b451ea550700e8c80
Markiesch <marksmeel@gmail.com>
2021-03-28 21:47:45 +0200

    Removed Log message feature

--- features/log-messages.js
+++
@@ -1,7 +0,0 @@
-// module.exports = (client) => {
-//     client.on("message", (message) => {
-//         if (message.author.bot) return;
-
-//         console.log(message.content);
-//     });
-// };
