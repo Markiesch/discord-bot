4207c14a39c732ccd1d9aeded7bb1c11c75f6f34
Markiesch <marksmeel@gmail.com>
2021-04-02 13:50:00 +0200

    Changed directorys

---
+++ commands/moderation/purge.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    category: "Utility",
+    description: "Deletes a specific amount of messages in a channel",
+    minArgs: 1,
+    expectedArgs: "<amount>",
+    syntaxError: `Incorrect Usage! Use {PREFIX}{COMMAND} {ARGUMENTS}`,
+    guildOnly: true,
+    requiredPermissions: ["ADMINISTRATOR"],
+    callback: ({ message, args }) => {
+        const amount = parseInt(args[0]) + 1;
+
+        const failMessage = new MessageEmbed().setColor("#f14948").setDescription(`<:failed:818800981001240617> Failed to clear **<#${amount}>** messages`);
+
+        if (isNaN(amount)) return message.reply("that doesn't seem to be a valid number.");
+        if (amount <= 1 || amount > 100) return message.reply("you need to input a number between 1 and 99.");
+
+        message.channel
+            .bulkDelete(amount, true)
+            .catch((err) => {
+                console.error(err);
+                message.channel.send(failMessage);
+            })
+            .then((msg) => {
+                const succesMessage = new MessageEmbed().setColor("#43b581").setDescription(`<:succes:818800870274891827> Deleted **${msg.size - 1}** message`);
+                message.channel.send(succesMessage).then((msg) => {
+                    msg.delete({ timeout: 5000 }).catch((err) => console.log(err));
+                });
+            });
+    },
+};
