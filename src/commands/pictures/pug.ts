241442436077949450e86e3968c3a99c5eb5b7ca
Markiesch <marksmeel@gmail.com>
2021-12-25 22:39:37 +0100

    Added pug command

---
+++ src/commands/pictures/pug.ts
@@ -0,0 +1,19 @@
+import { Message, MessageEmbed } from "discord.js";
+import command from "../command";
+import axios from "axios";
+
+export default class pug extends command {
+  description = "Gives you a random dog picture";
+
+  async execute(message: Message) {
+    try {
+      const res = await axios.get("https://dog.ceo/api/breed/pug/images/random");
+      const image = res.data.message;
+      const birdEmbed = new MessageEmbed().setColor("RANDOM").setTitle("🐶 Woef...").setURL(image).setImage(image);
+
+      message.reply({ embeds: [birdEmbed] });
+    } catch (error) {
+      if (error instanceof Error) console.log(error.message);
+    }
+  }
+}
