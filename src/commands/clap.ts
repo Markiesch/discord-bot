b6da8148a3b4420abde11eead13b9ffed27d1c1c
Markiesch <marksmeel@gmail.com>
2021-12-25 20:10:28 +0100

    Added clap command

---
+++ src/commands/clap.ts
@@ -0,0 +1,11 @@
+import { Message } from "discord.js";
+import command from "./command";
+
+export default class clap extends command {
+  description = "makes 👏 your 👏 messages 👏 look 👏 like 👏 this";
+
+  execute(message: Message, ...args: string[]) {
+    if (!args.length) return;
+    message.reply(args.join(" :clap: "));
+  }
+}
--- src/commands/ping.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "discord.js";
-
-export default class ping {
-  callback(message: Message, ...args: string[]) {
-    console.log(args);
-    message.reply("pong");
-  }
-}
