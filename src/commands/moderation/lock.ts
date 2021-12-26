ffdcb3a327d18313296d0cfec841bfb5a00f6d35
Markiesch <marksmeel@gmail.com>
2021-12-26 13:46:49 +0100

    Added lock command

---
+++ src/commands/moderation/lock.ts
@@ -0,0 +1,22 @@
+import { GuildChannel, Message, MessageEmbed } from "discord.js";
+import command from "../command";
+
+export default class lock extends command {
+  description = "Locks the mentioned channel";
+
+  execute(message: Message): void {
+    const targetChannel = (message.mentions.channels.first() || message.channel) as GuildChannel;
+    const failMessage = new MessageEmbed().setColor("#f14948").setDescription(`<:failed:818800981001240617> Failed to lock **<#${targetChannel.id}>**`);
+    const lockedMessage = new MessageEmbed().setColor("#43b581").setDescription(`:lock: **<#${targetChannel.id}>** has been locked`);
+
+    try {
+      targetChannel.permissionOverwrites.edit(message.guildId!, { SEND_MESSAGES: false });
+      if (message.client.user) targetChannel.permissionOverwrites.edit(message.client.user?.id, { SEND_MESSAGES: true });
+
+      message.channel.send({ embeds: [lockedMessage] });
+      message.delete();
+    } catch (error) {
+      message.reply({ embeds: [failMessage] });
+    }
+  }
+}
