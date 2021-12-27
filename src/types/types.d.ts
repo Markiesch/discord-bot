6ee1a5ced553e643da8e97d0e67a2d96800fedba
Markiesch <marksmeel@gmail.com>
2021-12-27 16:42:06 +0100

    Added permission check

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,8 +1,9 @@
-import { Message } from "discord.js";
+import { Client, Message, PermissionString, TextChannel } from "discord.js";
 
 export interface ICommand {
   name?: string;
   alias?: string[] | string;
   description: string;
-  execute: ({ message, args }: { message: Message; args: string[] }) => void;
+  permissions?: PermissionString | PermissionString[];
+  execute: ({ channel, message, args, client }: { channel: TextChannel; message: Message; args: string[]; client: Client }) => void;
 }
